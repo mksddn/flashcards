b8aafd28b0eae0310104419c43e361e47b9dd106
Max Dudin <22976310+mksddn@users.noreply.github.com>
2023-11-01 12:51:23 +0300

    clean js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
 let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
-		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+	fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
 import pkg from 'gulp'
 const { gulp, src, dest, parallel, series, watch } = pkg
 
-import browserSync   from 'browser-sync'
-import bssi          from 'browsersync-ssi'
-import ssi           from 'ssi'
+import browserSync from 'browser-sync'
+import bssi from 'browsersync-ssi'
+import ssi from 'ssi'
 import webpackStream from 'webpack-stream'
-import webpack       from 'webpack'
-import TerserPlugin  from 'terser-webpack-plugin'
-import gulpSass      from 'gulp-sass'
-import dartSass      from 'sass'
-import sassglob      from 'gulp-sass-glob'
-const  sass          = gulpSass(dartSass)
-import less          from 'gulp-less'
-import lessglob      from 'gulp-less-glob'
-import styl          from 'gulp-stylus'
-import stylglob      from 'gulp-noop'
-import postCss       from 'gulp-postcss'
-import cssnano       from 'cssnano'
-import autoprefixer  from 'autoprefixer'
-import imagemin      from 'gulp-imagemin'
-import changed       from 'gulp-changed'
-import concat        from 'gulp-concat'
-import rsync         from 'gulp-rsync'
-import {deleteAsync} from 'del'
+import webpack from 'webpack'
+import TerserPlugin from 'terser-webpack-plugin'
+import gulpSass from 'gulp-sass'
+import dartSass from 'sass'
+import sassglob from 'gulp-sass-glob'
+const sass = gulpSass(dartSass)
+import less from 'gulp-less'
+import lessglob from 'gulp-less-glob'
+import styl from 'gulp-stylus'
+import stylglob from 'gulp-noop'
+import postCss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+import imagemin from 'gulp-imagemin'
+import changed from 'gulp-changed'
+import concat from 'gulp-concat'
+import rsync from 'gulp-rsync'
+import { deleteAsync } from 'del'
 
 function browsersync() {
 	browserSync.init({
@@ -38,46 +38,6 @@ function browsersync() {
 		online: true,
 		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
 	})
-}
-
-function scripts() {
-	return src(['app/js/app.js'])
-		.pipe(webpackStream({
-			mode: 'production',
-			performance: { hints: false },
-			// plugins: [
-			// 	new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' }), // jQuery (npm i jquery)
-			// ],
-			// module: {
-			// 	rules: [
-			// 		{
-			// 			test: /\.m?js$/,
-			// 			exclude: /(node_modules)/,
-			// 			use: {
-			// 				loader: 'babel-loader',
-			// 				options: {
-			// 					presets: ['@babel/preset-env'],
-			// 					plugins: ['babel-plugin-root-import']
-			// 				}
-			// 			}
-			// 		}
-			// 	]
-			// },
-			// optimization: {
-			// 	minimize: true,
-			// 	minimizer: [
-			// 		new TerserPlugin({
-			// 			terserOptions: { format: { comments: false } },
-			// 			extractComments: false
-			// 		})
-			// 	]
-			// },
-		}, webpack)).on('error', (err) => {
-			this.emit('end')
-		})
-		.pipe(concat('app.min.js'))
-		.pipe(dest('app/js'))
-		.pipe(browserSync.stream())
 }
 
 function styles() {
@@ -104,11 +64,13 @@ function images() {
 function buildcopy() {
 	return src([
 		'{app/js,app/css}/*.min.*',
+		'app/js/*.js',
+		'app/js/db.json',
 		'app/images/**/*.*',
 		'!app/images/src/**/*',
 		'app/fonts/**/*'
 	], { base: 'app/' })
-	.pipe(dest('dist'))
+		.pipe(dest('dist'))
 }
 
 async function buildhtml() {
@@ -129,7 +91,7 @@ function deploy() {
 			destination: 'yousite/public_html/',
 			clean: true, // Mirror copy with file deletion
 			include: [/* '*.htaccess' */], // Included files to deploy,
-			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
+			exclude: ['**/Thumbs.db', '**/*.DS_Store'],
 			recursive: true,
 			archive: true,
 			silent: false,
@@ -139,13 +101,13 @@ function deploy() {
 
 function startwatch() {
 	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
-	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch(['app/js/**/*.js'], { usePolling: true })
 	watch('app/images/src/**/*', { usePolling: true }, images)
 	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
 }
 
-export { scripts, styles, images, deploy }
-export let assets = series(scripts, styles, images)
-export let build = series(cleandist, images, scripts, styles, buildcopy, buildhtml)
+export { styles, images, deploy }
+export let assets = series(styles, images)
+export let build = series(cleandist, images, styles, buildcopy, buildhtml)
 
-export default series(scripts, styles, images, parallel(browsersync, startwatch))
+export default series(styles, images, parallel(browsersync, startwatch))
